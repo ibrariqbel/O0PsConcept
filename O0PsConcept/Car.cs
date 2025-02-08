dfe4ccd53ecbcaf9d8345eac833f04cb0ba13f35
Dell Laptop <Dell Laptop@DESKTOP-2SKCF1H>
2025-02-08 15:40:20 +0500

    #Uncomment all car.cs file

--- O0PsConcept/Car.cs
+++ O0PsConcept/Car.cs
@@ -1,37 +1,37 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace O0PsConcept
-//{
-//    public class Car
-//    {
-//        // methhod
-//        public string Name { get; set; }
-//        public string BrandNName { get; set; }
-//        public int NoOfSeat { get; set; }
+namespace O0PsConcept
+{
+    public class Car
+    {
+        // methhod
+        public string Name { get; set; }
+        public string BrandNName { get; set; }
+        public int NoOfSeat { get; set; }
 
-//        //method
-//        public int Accelator(int pressure)
-//        {
-//            int speed = pressure * 10;
-//            return speed;
-//        }
+        //method
+        public int Accelator(int pressure)
+        {
+            int speed = pressure * 10;
+            return speed;
+        }
 
-//        public int Brake(int pressure)
-//        {
-//            int speed = pressure / 10;
-//            return speed;
-//        }
+        public int Brake(int pressure)
+        {
+            int speed = pressure / 10;
+            return speed;
+        }
 
-//        public int clush(int pressure)
-//        {
-//            int speed = pressure;
-//            return speed;
-//        }
+        public int clush(int pressure)
+        {
+            int speed = pressure;
+            return speed;
+        }
 
-//    }
+    }
 
-//}
+}
